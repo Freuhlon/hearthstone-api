d4c5e16cc40ab6530813c466dd852bb4bb407cc9
Angélo LIMA <angelomiguellima@gmail.com>
2020-11-29 07:35:01 +0100

    chore: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from './config/config.service';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 async function bootstrap() {
   const configService = new ConfigService('.env');
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  const options = new DocumentBuilder()
+      .setTitle('Hearthstone cards API')
+      .setDescription('List of all Heathstone cards APIs')
+      .setVersion('1.0')
+      .addTag('cards')
+      .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
   await app.listen(configService.get('NODE_PORT'));
 }
 
--- src/search/controller/card.controller.ts
+++ src/search/controller/card.controller.ts
@@ -1,14 +1,19 @@
-import { Controller, Get, Query } from '@nestjs/common';
-import { CardService } from '../service/card.service';
+import {Controller, Get, Query} from '@nestjs/common';
+import {CardService} from '../service/card.service';
+import {ApiOperation, ApiQuery, ApiResponse, ApiTags} from "@nestjs/swagger";
 
+@ApiTags('cards')
 @Controller('cards')
 export class CardController {
 
-  constructor(private readonly cardService: CardService) {
-  }
+    constructor(private readonly cardService: CardService) {
+    }
 
-  @Get()
-  search(@Query('query') query = "") {
-    return this.cardService.search(query);
-  }
+    @Get()
+    @ApiOperation({summary: "Search cards through a query, empty query retrieves all cards."})
+    @ApiResponse({status: 200, description: 'Cards has been successfully found.'})
+    @ApiQuery({ name: 'query', allowEmptyValue: true, description: 'ESQuery' })
+    search(@Query('query') query = "") {
+        return this.cardService.search(query);
+    }
 }
